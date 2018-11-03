6330fb77df6227fc1c7be4ca29deacd293ee2823
pabloleonalcaide <pabloleonpsico@gmail.com>
2018-11-03 17:19:37 +0100

    new assets

--- _src/js/data-scroll.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready( () => {
-    $( "[data-scroll]" ).click( function( e ) {
-        e.preventDefault();
-
-        $( "html, body" ).animate({
-            scrollTop: $( "[data-section='" + $( this ).attr( 'data-scroll' ) + "']" ).offset().top - 64
-        }, 1000 );
-    });
-});
